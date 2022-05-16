67facc4ad08513f4e5890cd8444015c4e22f50e9
anndfelix <anndfelix@gmail.com>
2022-05-15 22:27:32 -0700

    Revert "Merge pull request #17 from JorgeMasson/fragmentos"
    This reverts commit f1bdcfc80fdcc7f5b1c711e66da88245a4a59b49, reversing
    changes made to a96623cc92bd8fe04595090818c3e7487d0ad3d4.

--- app/src/main/java/masson/reynoso/tofi/LoginActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/LoginActivity.kt
@@ -1,113 +1,38 @@
 package masson.reynoso.tofi
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity : AppCompatActivity() {
-
-    private lateinit var auth: FirebaseAuth
-    private lateinit var mPreferences: SharedPreferences
-    private lateinit var mEditor: SharedPreferences.Editor
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
         val btnLogin = findViewById<Button>(R.id.btnInicio)
         val btnRegistro = findViewById<Button>(R.id.btnRegistro)
+        val btnOlvidarContraseña = findViewById<TextView>(R.id.txtOlvidaPass)
 
-        //////////////////////////////REMEMBERME////////////////////////////////////
-
-        // Recuperamos el contenido de los textField
-        val mName = findViewById<EditText>(R.id.inputUsuarioLogin)
-        val mCheckBox = findViewById<CheckBox>(R.id.btnRecordarUser)
-
-        val sp = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-
-
-
-        ////////////////////////////////////////////////////////////////////////////
-
-        btnLogin.setOnClickListener {
-            val lanzar = Intent(this, ProfilesActivity::class.java)
+        btnLogin.setOnClickListener{
+            val lanzar = Intent(this, ProfilesActivity:: class.java)
             startActivity(lanzar)
-
-//            valida_ingreso()
         }
 
-        btnRegistro.setOnClickListener {
-            val lanzar = Intent(this, CreateaccActivity::class.java)
+        btnRegistro.setOnClickListener{
+            val lanzar = Intent(this, ConfiguraPerfilActivity:: class.java)
+            startActivity(lanzar)
+        }
+
+        btnOlvidarContraseña.setOnClickListener{
+            val lanzar = Intent(this, RecoveryActivity:: class.java)
             startActivity(lanzar)
         }
 
     }
 
-
-
-    private fun valida_ingreso() {
-        val et_correo: EditText = findViewById(R.id.inputUsuarioLogin)
-        val et_contra: EditText = findViewById(R.id.inputPassLogin)
-
-
-        var correo: String = et_correo.text.toString()
-        var contra: String = et_contra.text.toString()
-
-        /////////////////////////////----------------REMEMBERME------------------/////////////////////////////
-
-
-        /////////////////////////////-----------------------------------/////////////////////////////
-
-        if (!correo.isNullOrBlank() && !contra.isNullOrBlank()) {
-            //ingresa a la Firebase
-
-            ingresaFirebase(correo, contra)
-
-        } else {
-            Toast.makeText(this, "Ingresar datos!!", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun ingresaFirebase(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    //Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    //updateUI(user)
-
-                    val intent: Intent = Intent(this, ProfilesActivity::class.java)
-                    startActivity(intent)
-
-                } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    //updateUI(null)
-                }
-            }
-    }
-
-
-    fun clickTextView(view: View?) {
-        val lanzar = Intent(this, RecoveryActivity::class.java)
-        startActivity(lanzar)
-    }
 }
--- app/src/main/java/masson/reynoso/tofi/MainActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/MainActivity.kt
@@ -3,43 +3,21 @@ package masson.reynoso.tofi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
-import masson.reynoso.tofi.databinding.ActivityMainBinding
-import java.util.*
+import android.widget.ImageView
 
 
 public class MainActivity : AppCompatActivity() {
 
-    var binding: ActivityMainBinding? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+        val tofi = findViewById<ImageView>(R.id.btnPrueba)
 
-
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
-//        val boton1 = findViewById<Button>(R.id.btnPrueba)
-//
-//        boton1.setOnClickListener{
-//            val lanzar = Intent(this, LoginActivity:: class.java)
-//            startActivity(lanzar)
-//        }
-
-        //CONFIGURA PERFIL GRID VIEW
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-
-        val addProfile = arrayOf("Agregar Perfil")
-        val addPerfilImages = intArrayOf(R.drawable.agregarperfil)
-
+        tofi.setOnClickListener{
+            val login = Intent(this,LoginActivity::class.java)
+            startActivity(login)
+        }
     }
-}
+} 
