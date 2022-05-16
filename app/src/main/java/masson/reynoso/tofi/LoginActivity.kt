a96623cc92bd8fe04595090818c3e7487d0ad3d4
anndfelix <anndfelix@gmail.com>
2022-05-15 21:54:40 -0700

    pantallas actualizadas

--- app/src/main/java/masson/reynoso/tofi/LoginActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +16,7 @@ class LoginActivity : AppCompatActivity() {
 
         val btnLogin = findViewById<Button>(R.id.btnInicio)
         val btnRegistro = findViewById<Button>(R.id.btnRegistro)
+        val btnOlvidarContraseña = findViewById<TextView>(R.id.txtOlvidaPass)
 
         btnLogin.setOnClickListener{
             val lanzar = Intent(this, ProfilesActivity:: class.java)
@@ -26,10 +28,11 @@ class LoginActivity : AppCompatActivity() {
             startActivity(lanzar)
         }
 
+        btnOlvidarContraseña.setOnClickListener{
+            val lanzar = Intent(this, RecoveryActivity:: class.java)
+            startActivity(lanzar)
+        }
+
     }
 
-    fun clickTextView(view: View?) {
-        val lanzar = Intent(this, RecoveryActivity:: class.java)
-        startActivity(lanzar)
-    }
 }
--- app/src/main/java/masson/reynoso/tofi/MainActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/MainActivity.kt
@@ -4,20 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 
 public class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val boton1 = findViewById<Button>(R.id.btnPrueba)
+        val tofi = findViewById<ImageView>(R.id.btnPrueba)
 
-        boton1.setOnClickListener{
-            val lanzar = Intent(this, Inicio:: class.java)
-            startActivity(lanzar)
+        tofi.setOnClickListener{
+            val login = Intent(this,LoginActivity::class.java)
+            startActivity(login)
         }
     }
-}
+} 
