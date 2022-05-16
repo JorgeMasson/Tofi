d404bd5ea8df16a6beab836e556100668b448c8a
anndfelix <anndfelix@gmail.com>
2022-05-15 22:30:48 -0700

    Revert "Merge pull request #18 from JorgeMasson/prueba"
    This reverts commit ba33e46d8090278e5cccad4d223151c238e7df71, reversing
    changes made to 9859368bacfe6f4850293127834791daa027eef1.

--- app/src/main/java/masson/reynoso/tofi/MainActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/MainActivity.kt
@@ -3,21 +3,43 @@ package masson.reynoso.tofi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
-import android.widget.ImageView
+import com.google.firebase.auth.FirebaseAuth
+import masson.reynoso.tofi.databinding.ActivityMainBinding
+import java.util.*
 
 
 public class MainActivity : AppCompatActivity() {
 
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        val tofi = findViewById<ImageView>(R.id.btnPrueba)
+        supportActionBar?.hide()
 
-        tofi.setOnClickListener{
-            val login = Intent(this,LoginActivity::class.java)
-            startActivity(login)
-        }
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+//        val boton1 = findViewById<Button>(R.id.btnPrueba)
+//
+//        boton1.setOnClickListener{
+//            val lanzar = Intent(this, LoginActivity:: class.java)
+//            startActivity(lanzar)
+//        }
+
+        //CONFIGURA PERFIL GRID VIEW
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val addProfile = arrayOf("Agregar Perfil")
+        val addPerfilImages = intArrayOf(R.drawable.agregarperfil)
+
     }
-} 
+}
--- app/src/main/java/masson/reynoso/tofi/ProfilesActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/ProfilesActivity.kt
@@ -3,11 +3,34 @@ package masson.reynoso.tofi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
+import masson.reynoso.tofi.databinding.ActivityProfilesBinding
 
 class ProfilesActivity : AppCompatActivity() {
+    var binding: ActivityProfilesBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profiles)
+        binding = ActivityProfilesBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+
+        val profileName = arrayOf("Add Profile");
+        val profileImage = intArrayOf(R.drawable.agregarperfil);
+        val gridAdapter = GridAdapter(this@ProfilesActivity, profileName, profileImage);
+
+
+        binding!!.gridView.adapter = gridAdapter
+        binding!!.gridView.onItemClickListener =
+
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+
+                if (position == 0) {
+                    val lanzar = Intent(this, ConfiguraPerfilActivity::class.java)
+                    startActivity(lanzar)
+                } else {
+                    Toast.makeText(this@ProfilesActivity, "You Clicked on " + profileName[0], Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
