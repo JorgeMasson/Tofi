f7aba74fcf618d18013959d9851671cfe99d6556
anndfelix <anndfelix@gmail.com>
2022-05-15 22:34:36 -0700

    Revert "Merge pull request #16 from JorgeMasson/develop"
    This reverts commit c9965ebb84cb5a79996dcec844a8ed6ca0b17c17, reversing
    changes made to c785f19dbaa70ec83b0f9293dce9cff44a79657b.

--- app/src/main/java/masson/reynoso/tofi/RecoveryPassActivity.kt
+++ app/src/main/java/masson/reynoso/tofi/RecoveryPassActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 
 class RecoveryPassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,16 +11,10 @@ class RecoveryPassActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recoverypass)
 
         val btnReestablecer = findViewById<Button>(R.id.btnReestablecer)
-        val imgDevolver = findViewById<ImageButton>(R.id.ibRegresar)
-        imgDevolver.setOnClickListener{
-            val lanzar = Intent(this, RecoveryActivity:: class.java)
-            startActivity(lanzar)
-        }
+
         btnReestablecer.setOnClickListener{
             val lanzar = Intent(this, ProfilesActivity:: class.java)
             startActivity(lanzar)
         }
-
-
     }
 }
